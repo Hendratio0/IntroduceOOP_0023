8cb121da21ee6879c608700fc1275561e34a5f0c
Hendratio0 <hendraaatio@gmail.com>
2025-04-28 11:16:55 +0700

    commit sembilan

--- class2.cpp
+++ class2.cpp
@@ -7,4 +7,21 @@ private:
     int panjang;
     int lebar;
     int luas;
+
+public:
+    void input_data()
+    {
+        cout << "Masukkan panjang = ";
+        cin >> panjang;
+        cout << "Masukkan lebar = ";
+        cin >> lebar;
+    }
+    void output_data()
+    {
+        cout << "tampilkan luas = " << luas << endl;
+    }
+    void hitung_luas()
+    {
+        luas = panjang * lebar;
+    }
 };
